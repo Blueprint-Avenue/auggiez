ee90c10796178363f2c212653b4ed5617892ebc5
Samiya <skmcintosh20@gmail.com>
2022-05-03 20:11:19 -0700

    finished thumbnail component and css

---
+++ components/Movies.js
@@ -0,0 +1,13 @@
+import Thumbnail from "./Thumbnail";
+
+function Movies({results}) {
+	return (
+		<div>
+			{results.map((result) => (
+				<Thumbnail key={result.id} result={result} />
+			))}
+		</div>
+	);
+}
+
+export default Movies;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import {Header} from "../components/Header";
 import Navbar from "../components/Navbar";
+import Movies from "../components/Movies";
+import requests from "../utils/requests";
 
-export default function Home() {
+// Rendered for Client
+export default function Home({results}) {
 	return (
 		<div>
 			<Head>
@@ -14,7 +17,25 @@ export default function Home() {
 			<Header />
 			{/* Navbar */}
 			<Navbar />
-			{/* Body */}
+			{/* Movie Body */}
+			<Movies results={results} />
 		</div>
 	);
+}
+
+// Rendered for Server - Happens first then rendered to the client
+export async function getServerSideProps(context) {
+	const genre = context.query.genre;
+
+	const request = await fetch(
+		`https://api.themoviedb.org/3${
+			requests[genre]?.url || requests.fetchTrending.url
+		}`
+	).then((res) => res.json());
+
+	return {
+		props: {
+			results: request.results,
+		},
+	};
 }
